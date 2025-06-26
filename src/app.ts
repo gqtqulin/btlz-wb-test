bcc6c4b2e086d7797579f50e6170f007bf65ecf2
Oleg Gataulin <gqtqulin@icloud.com>
2025-06-26 21:24:47 +0300

    add commander

--- src/app.ts
+++ src/app.ts
@@ -4,3 +4,16 @@ await migrate.latest();
 await seed.run();
 
 console.log("All migrations and seeds have been run");
+
+// https://common-api.wildberries.ru/api/v1/tariffs/box
+
+
+/**
+ * 
+ * регулярное получение информации о тарифах wb и сохранение их в БД на каждый день;
+ * 
+ * регулярное обновление информации о актуальных тарифах в google-таблицах.
+ * 
+ * 
+ */
+
